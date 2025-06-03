9fc208186ecaaaa679f98bb85dee4739ca41b38c
Divyanshu <Divyanshu@pradyant01.cdacnoida.in>
2025-06-03 13:53:59 +0530

    CDAC Updated

--- master-frontend/src/index.jsx
+++ master-frontend/src/index.jsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+
+// EndPoint for Leaves Master =>        http://localhost:5173/leave                (UPDATED)
+// EndPoint for User Master =>          http://localhost:5173/user-type            (UPDATED)
+// EndPoint for Designation Master =>   http://localhost:5173/designation          (UPDATED)
+// EndPoint for Qualification Master => http://localhost:5173/qualification        (UPDATED)
+// EndPoint for University Master =>    http://localhost:5173/university-type      (UPDATED)
+// EndPoint for Institute Master =>     http://localhost:5173/institute            (UPDATED)

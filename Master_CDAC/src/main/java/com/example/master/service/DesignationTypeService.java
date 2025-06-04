2f731776003de46da37718e5c276b1f724210028
Divyanshu <Divyanshu@pradyant01.cdacnoida.in>
2025-06-04 11:50:23 +0530

    Minor Update

--- Master_CDAC/src/main/java/com/example/master/service/DesignationTypeService.java
+++ Master_CDAC/src/main/java/com/example/master/service/DesignationTypeService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public class DesignationTypeService {
-    @Autowired
+
     private final DesignationTypeRepository repository;
 
     public DesignationTypeService(DesignationTypeRepository repository) {
